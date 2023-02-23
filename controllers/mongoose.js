c2619d85f080a5ad6aed76c09b2fa54a9fbc1763
Anurag Srivastav <anuragsrivastav0027@gmail.com>
2023-02-23 16:17:38 +0530

    added cloud db

--- controllers/mongoose.js
+++ controllers/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose');
 mongoose.set("strictQuery", false);
-mongoose.connect("mongodb://0.0.0.0:27017/Codemon")
+mongoose.connect("mongodb+srv://anurag-327:yZMFEZ9zq5a6YLzN@codemon.mtoc0me.mongodb.net/codemon?retryWrites=true&w=majority")
 .then(()=> console.log("Database connected sucessfully"))
 .catch(err => console.log("error setting up database"));
