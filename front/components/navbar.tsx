0581a707472388c010701bc6c944a595a0ed5c67
Anurag Srivastav <anuragsrivastav0027@gmail.com>
2023-10-26 19:09:02 +0530

    Resolved deployment issues

--- front/components/navbar.tsx
+++ front/components/navbar.tsx
@@ -1,7 +1,10 @@
 import { useStore } from "@/zustand/useStore";
 import Avatar from "./avatar";
 import { CodeBlock } from "@phosphor-icons/react/dist/ssr";
+import { removeToken } from "@/helper/tokenhandler";
+import { useRouter,useSearchParams } from "next/navigation";
 const Navbar = () => {  
+  const router=useRouter();
   const user = useStore((state) => state.user);
   return (
     <nav className="fixed top-0 z-10 flex items-center justify-between w-full py-3 bg-white shadow-md p">
@@ -15,8 +18,8 @@ const Navbar = () => {
              <a className="px-2 py-1 underline" href="/">Home</a>
              <a className="px-2 py-1 underline" href="/compiler">Compiler</a>
              {
-                  user?<a className="px-3" href={`/users/${user.username}`}><Avatar value="Anurag Srivastav" />
-                    </a>:<a className="px-6 py-1 text-white bg-red-500 rounded-md" href="/login">Login</a>
+                  user?<div className="flex items-center gap-2"><span onClick={() =>{ removeToken(); router.push("/</>")}}>Logout</span><a className="px-3 mr-2" href={`/users/${user.username}`}><Avatar value="Anurag Srivastav" />
+                  </a></div>:<a className="px-6 py-1 text-white bg-red-500 rounded-md" href="/login">Login</a>
              }
              
              
