13190050e44710686687d2f871b3ebcad2c20d9f
Anurag Srivastav <anuragsrivastav0027@gmail.com>
2024-02-08 15:27:50 +0530

    updated editor options

--- front/helper/Editor.ts
+++ front/helper/Editor.ts
@@ -1,35 +1,10 @@
-export const EditorOptions = {
-    autoIndent: "full",
-    contextmenu: true,
+export const EditorOptions = { 
     fontFamily: "monospace",
     fontSize: 13,
-    lineHeight: 20,
-    matchBrackets: "always",
-    minimap: {
-      enabled: false,
-    },
-    scrollbar: {
-      horizontalSliderSize: 4,
-      verticalSliderSize: 14,
-    },
-    selectOnLineNumbers: true,
-    readOnly: false,
-    cursorStyle: "line",
   };
 export const ReadOnlyEditorOptions = {
-    autoIndent: "full",
-    contextmenu: true,
+    
     fontFamily: "monospace",
     fontSize: 13,
-    lineHeight: 20,
-    minimap: {
-      enabled: true,
-    },
-    scrollbar: {
-      horizontalSliderSize: 4,
-      verticalSliderSize: 14,
-    },
-    selectOnLineNumbers: true,
     readOnly: true,
-    cursorStyle: "line",
   };
